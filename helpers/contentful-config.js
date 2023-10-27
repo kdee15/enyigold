6670f99727ac3837f7af30ae12dbd763869feba9
Kirk Daniels <kirkdaniels@me.com>
2023-10-27 22:18:25 +0200

    setup

--- helpers/contentful-config.js
+++ helpers/contentful-config.js
@@ -1,5 +1,5 @@
-const C_SPACE_ID = CONTENTFUL_SPACE_ID;
-const C_DELIVERY_KEY = CONTENTFUL_DELIVERY_KEY;
+const C_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+const C_DELIVERY_KEY = process.env.CONTENTFUL_DELIVERY_KEY;
 const C_GRAPHQL_URL = `https://graphql.contentful.com/content/v1/spaces/${C_SPACE_ID}/environments/master`;
 
 module.exports = {
